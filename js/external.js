3a919a0173f59ac44f0d13cc0d92235f74d617dd
Darius Brown <brown.darius.r@gmail.com>
2021-02-02 10:21:37 -0600

    Internal and external javascript ver.1

---
+++ js/external.js
@@ -0,0 +1,12 @@
+"use strict";
+    console.log (`Hello from inline Javascript`);
+    // JavaScript code goes here
+        alert("Welcome to the website.");
+    var userInput = prompt("What is your favorite color?");
+        alert("Great! " + userInput + " is my favorite color too!");
+    var userInput =prompt("How many days did you rent The Little Mermaid?");
+        alert("The total is $" + userInput * 3)
+    var userInput =prompt("How many days did you rent Brother Bear?");
+        alert("The total is $" + userInput * 3)
+    var userInput =prompt("How many days did you rent Hercules?");
+        alert("The total is $" + userInput * 3 )
