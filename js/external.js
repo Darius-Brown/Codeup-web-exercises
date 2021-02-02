fbd9ed9c5e0a86ac36044ad7b01547f3f94ecaff
Darius Brown <brown.darius.r@gmail.com>
2021-02-02 16:26:21 -0600

    external js V2

--- js/external.js
+++ js/external.js
@@ -1,12 +1,44 @@
 "use strict";
-    console.log (`Hello from inline Javascript`);
-    // JavaScript code goes here
         alert("Welcome to the website.");
-    var userInput = prompt("What is your favorite color?");
-        alert("Great! " + userInput + " is my favorite color too!");
-    var userInput =prompt("How many days did you rent The Little Mermaid?");
-        alert("The total is $" + userInput * 3)
-    var userInput =prompt("How many days did you rent Brother Bear?");
-        alert("The total is $" + userInput * 3)
-    var userInput =prompt("How many days did you rent Hercules?");
-        alert("The total is $" + userInput * 3 )
+    var favColor = prompt("What is your favorite color?");
+        alert("Great! " + favColor + " is my favorite color too!");
+
+    var mermaid =Number(prompt("How many days did you rent The Little Mermaid?"));
+
+    var bear =Number(prompt("How many days did you rent Brother Bear?"));
+
+    var herc =Number(prompt("How many days did you rent Hercules?"));
+
+        alert("The total price is" + mermaid + bear + herc *3 );
+
+    var hoursGoogle = Number(prompt("How many hours did you work at Google"));
+
+    var hoursAmazon = Number(prompt("How many hours did you work at Amazon"));
+
+    var hoursFacebook = Number(prompt("How many hours did you work at Facebook"));
+
+    var payGoogle = Number(alert("You earned $" + hoursGoogle *400 ));
+
+    var payAmazon = Number(alert("You earned $" + hoursAmazon *380 ));
+
+    var payFacebook = Number(alert("You earned $" + hoursFacebook *350 ));
+
+    var totalPay = Number(payFacebook + payAmazon + payGoogle);
+        alert("Your total pay this week is $" + totalPay);
+
+
+
+    var enrolled = prompt("Are you currently enrolled? \(answer with 'yes' or 'no'\)");
+    var schedFull = prompt("Is your current schedule full? \(answer with 'yes' or 'no'\)");
+    var seats = prompt("Are there any remaining seats in this class\(answer with 'yes' or 'no'\)");
+        alert("You are now enrolled.");
+    var itemCount = prompt("Enter number of items in cart");
+    var expired = prompt("Are any of the items currently expired?");
+    var premium = prompt("Are you a premium member?");
+        alert("discount applied");
+
+
+
+
+
+
