ac15709cd4395f619975dc0b7f630afe1dec447b
Darius Brown <brown.darius.r@gmail.com>
2021-02-13 23:06:19 -0600

    break

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -15,6 +15,16 @@
 */
 var userNumber = prompt("Please enter an odd number between 1 & 50");
 
+for (var i = 1; i >= 50; i++) {
+
+} if (i % 2 === 1) {
+    console.log(i)
+} else if (userNumber = i) {
+    continue;
+}
+
+
+
 
 /**
 
@@ -45,39 +55,5 @@ Here is an odd number: 43
 Here is an odd number: 45
 Here is an odd number: 47
 Here is an odd number: 49
-While Loops
-Create a file named while.js in the js directory.
 
-    Create a while loop that uses console.log() to create the output shown below.
-
-
-2
-4
-8
-16
-32
-64
-128
-256
-512
-1024
-2048
-4096
-8192
-16384
-32768
-65536
-Do While Loop
-An ice cream seller can't go home until she sells all of her cones. First write enough code that generates a random number between 50 and 100 representing the amount of cones to sell before you start your loop. Inside of the loop your code should generate another random number between 1 and 5, simulating the amount of cones being bought by her clients. Use a do-while loop to log to the console the amount of cones sold to each person. This is a way get the random numbers for this exercise.
-
-
-// This is how you get a random number between 50 and 100
-var allCones = Math.floor(Math.random() * 50) + 50;
-// This expression will generate a random number between 1 and 5
-Math.floor(Math.random() * 5) + 1;
-The output should be similar to the following:
-
-    5 cones sold...  // if there are enough cones
-Cannot sell you 6 cones I only have 3...  // If there are not enough cones
-Yay! I sold them all! // If there are no more cones
  */
