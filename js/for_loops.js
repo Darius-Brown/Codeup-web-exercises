847c49929f17f6b281156e59c5bf667210a1a9b2
Darius Brown <Brown.darius.r@gmail.com>
2021-02-22 14:05:18 -0600

    loops

---
+++ js/for_loops.js
@@ -0,0 +1,14 @@
+
+
+console.log("hello loops");
+
+//
+
+function showMultiplicationTable(num){
+    for (var i = 1 ; i <= 10; i++){
+        //
+        console.log(num +" x " + i + " = " + (num * i));
+    }
+}
+
+showMultiplicationTable(7);
