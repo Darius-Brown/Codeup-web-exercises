d0c6d29ef1bfb460563672f77a09da037c4d8ab9
Darius Brown <Brown.darius.r@gmail.com>
2021-02-25 18:28:20 -0600

    git error uploaded cods to wrong branch (re upload)

--- js/while.js
+++ js/while.js
@@ -4,5 +4,24 @@ while (number <= 65536){
     console.log(number);
     number *= 2;
 }
+//Do whle exercise
 
+var aCones = Math.floor(Math.random() * 50) + 50;
+console.log("You have " + aCones + " cones.")
 
+do {
+    var requestedCones = Math.floor(Math.random() * 5) + 1;
+
+    if (requestedCones <= aCones){
+        // this is sell cones
+        aCones = aCones - requestedCones;
+        console.log("You sold " + requestedCones + " cones, and have " + aCones +" cones left to sell.")
+
+    }else{
+        //this is not enough cones to sell
+        console.log("Sorry, I don't have " + requestedCones + " cones, I only have " + aCones);
+    }
+
+}while (aCones > 0)
+
+console.log("We sold out on cones today!");
