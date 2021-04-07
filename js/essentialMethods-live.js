ca5f4733f54830a2f8ba5c4dae4cf5808858c9db
Darius Brown <Brown.darius.r@gmail.com>
2021-04-07 11:57:53 -0500

    inteliJ cmmit

--- js/essentialMethods-live.js
+++ js/essentialMethods-live.js
@@ -4,45 +4,80 @@
 //.html() - GET the HTML contents or SET the HTML contents
 
 //TODO: Let's get the HTML from the element with the ID of "best-seller-cont"
+console.log($('#best-seller-cont').html());
+
 
 //TODO together: Let's set up a string with HTML to replace this old list with our updated best sellers - FEDORAS have moved into the number one spot!
 
+var html = " ";
+
+html = "<h4 class='best-seller-header'>Our top sellers</h4>" +
+    "<ol><li>Fedora Hats</li>" +
+    "<li>Baseball Hats</li>" +
+    "<li>Cowboys Hats</li></ol>"
+
 //TODO: Now, let's get back into that "best-seller-cont" and replace that HTML!
 
+$("#best-seller-cont").html(html);
+
 //TODO Together: Can we make a click event that changes "Howell's" to "Marco's" when we click on it? Help me out!
+
+$(".navbar-brand").click(function (){
+    $(this).html("Marco's <i class=\"bi-shop mx-1\"></i>")
+});
 
 //.css() - get info about a property or change a property
 
 //TODO: Let's see what the width of our "main-content" class is with .css()
-
+console.log($('.main-content').css('width'));
 //TODO Together: Can you help me? I'd like to set my modal's text to have a cool color :)
-
+$('.modal-body').css('color','blue');
 //TODO: Let's make an object for us to use with .css(). .
-
+var warningRed = {
+    'background-color':'red',
+    'font-size':'72px'
+}
 //TODO Together: Nice - now, let's test this out by targeting our h1 with this warning background! Next, let's chain a font color after it
+
+$("h1").css(warningRed).css('background-color', 'cornflowerblue');
 
 //.addClass() ~ .removeClass() ~ .toggleClass()
 
 //TODO: We've used a lot of bootstrap on the page - let's see what we can use .addClass to do in combination with bootstrap. Let's UPPERCASE that h1 with the shiny red background!
-
+$("h1").addClass("text-uppercase")
 //TODO Together: Can you assist me in removing the "bg-dark" background from our FOOTER?
-
+$('footer').removeClass("bg-dark");
 //TODO: I have a custom css class known as hi-lite - .toggleClass() with a .click() would be a neat way to use it
-
-
+/*
+$('*').click(function (e){
+    e.preventDefault();
+    var ele = $(e.target);
+    ele.toggleClass('hi-lite');
+})
+*/
 //Bonus activity:
 
 //TODO: Create a variable to store HTML - use jQuery to get the entire nav element's inner HTML. Console.log your variable to confirm the contents.
-
+var html = $('nav').html();
+console.log(html);
 //TODO: Remove the class of bg-dark and navbar-dark from the <nav> with jQuery.
 
+$('nav').removeClass("bg-dark", "navbar-dark");
+
 //TODO: Add the class of navbar-light and bg-info from the <nav> with jQuery.
-
+$("nav").addClass("navbar-light", 'bg-info')
 //TODO: Finally, create the following - add your own button somewhere on the navbar. Add an event that will toggle the navbar between bg-dark/navbar-dark and bg-info/navbar-light respectively
-
+$('#colorschemeBtn').click(function (e){
+    //e.preventDefault();
+    var ele = $('nav');
+    ele.toggleClass('bg-dark');
+})
 //TODO: Create your own object with multiple CSS properties you'd like to change. Change whatever element(s) you had in mind with your CSS object and .css()!
 
-
+$('.container').css({
+    'color':'red',
+    'background-color': 'white'
+})
 
 //traversing
 
