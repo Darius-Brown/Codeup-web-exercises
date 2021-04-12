d1ecfd1252b76bbfedc857932098cfe19d072bc6
Darius Brown <Brown.darius.r@gmail.com>
2021-04-12 10:22:50 -0500

    inteliJ cmmit

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -1,17 +1,32 @@
 mapboxgl.accessToken = mapboxToken;
 
-var mapOptions = {
-    container: "map",
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-98.4861, 29.4252],
-    zoom: 8
-}
+// var mapOptions = {
+//     container: "map",
+//     style: 'mapbox://styles/mapbox/streets-v11',
+//     center: [-98.4861, 29.4252],
+//     zoom: 8
+// }
 
-var map = new mapboxgl.Map({
+var pizzaMap = new mapboxgl.Map({
      container: 'map', // container ID
      style: 'mapbox://styles/mapbox/streets-v11', // style URL
-     center: [-98.4861, 29.4252], // starting position [lng, lat]
+     center: [-98.618436, 29.593404], // starting position [lng, lat]
      zoom: 14// starting zoom
  });
 
-var pizzaMap = new mapboxgl.Map(mapOptions);
+// var map = new mapboxgl.Map(mapOptions);
+
+var pizzaMarker = new mapboxgl.Marker({color: "gold"})
+    .setLngLat([-98.618436, 29.593404])
+    .addTo(pizzaMap)
+
+var pizzaPopup = new mapboxgl.Popup()
+    .setHTML("<p>Grimaldi's The best Pizza in San Antonio</p>")
+// .addTo(pizzaMap)
+
+pizzaMarker.setPopup(pizzaPopup)
+
+ geocode("15900 La Cantera Pkwy Ste 25180, San Antonio, TX 78256", mapboxToken).then(function(results){
+     console.log(results);
+     marcoMap.flyTo(results);
+ })
