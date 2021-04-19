adbf3bb8396ef8b1565b5b229c8505f04f910003
Darius Brown <Brown.darius.r@gmail.com>
2021-04-19 09:09:16 -0500

    es6 lecture

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,92 @@
+"use strict";
+
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+    let min3 = users.filter(function (user){
+        if (user.languages.length >= 3) {
+            return user;
+        }
+    })
+console.log(min3);
+
+
+
+    let emails = users.map(function (user){
+        return user.email;
+    })
+console.log(emails);
+
+
+
+let yearsSum = users.reduce(function(total, user){
+    return user.yearsOfExperience + total;
+},0)
+console.log(yearsSum);
+
+
+
+let longest = users.reduce(function (highest, user){
+    let currentLength = user.email.length;
+        console.log(currentLength);
+    if (currentLength >= 18) {
+        return highest += user.email;
+    } else { return highest += ""};
+}," ")
+console.log(longest);
+
+
+
+
+
+    let nameList = users.reduce(function(string,user){
+            //console.log(user.name);
+        string = (user.name + ", ") + string;
+            //console.log(string);
+        return string;
+}," ");
+
+console.log(nameList);
+
+
+
+// Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
+//     Use .map to create an array of strings where each element is a user's email address
+// Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
+//     Use .reduce to get the longest email from the list of users.
+//     Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
